5105d18e40a930f604083bb0617506abef220c33
Tj Hom <tjhom@tred.com>
2017-07-18 16:12:16 -0700

    Homework Turn In
    Got a little hung up in the exact array, push protocal. Snuck into Adam's repo to finish the project and disect. Feeling alot more confident.

--- FEWD_Week_6/Assignment/starter_code/Js/index.js
+++ FEWD_Week_6/Assignment/starter_code/Js/index.js
@@ -1,28 +1,45 @@
 //change Backround Functions functions
+var whatCity = ['NYC', 'SF', 'LA', 'ATX', 'SYD']
+//image selection array
+var imageOptions = [];
+var mainDropDown;
 
-function showAustin(){
-	$('body').css('background-image', 'url(images/austin.jpg)');
-}
-//write showLa
-function showLa(){
-	$('body').css('background-image', 'url(images/la.jpg)');
-}
-//write showNyc
-function showNyc(){
-	$('body').css('background-image', 'url(images/nyc.jpg)');
-}
-//write showSf
-function showSf(){
-	$('body').css('background-image', 'url(images/sf.jpg)');
-}
-//write showSydney
-function showSydney(){
-	$('body').css('background-image', 'url(images/sydney.jpg)');
+// //Needs to be refactored
+// eliminated with a pushed array?
+
+// function showAustin(){
+// 	$('body').css('background-image', 'url(images/austin.jpg)');
+// }
+// function showLa(){
+// 	$('body').css('background-image', 'url(images/la.jpg)');
+// }
+// function showNyc(){
+// 	$('body').css('background-image', 'url(images/nyc.jpg)');
+// }
+// function showSf(){
+// 	$('body').css('background-image', 'url(images/sf.jpg)');
+// }
+// function showSydney(){
+// 	$('body').css('background-image', 'url(images/sydney.jpg)');
+// }
+
+//Get input
+function changeCity(){
+mainDropDown = document.getElementById('city-type').selectedIndex;
+$('body').css('background-image', 'url(images/'+imageOptions[mainDropDown]+')');
+console.log('city-type');
 }
 
 //add this array [nyc,sf,syd,atx,la].foreach(function(city){
-
-$(document).ready(function(){
+	//benifit to for vs For Reach?
 	
+$(document).ready(function(){
+	for(i=0; i < whatCity.length; i++){
+		$('city-type').append('option'+whatCity[i]+"</option>")
+	}
+
+imageOptions.push(src = "citipix_skyline.jpg", "nyc.jpg", src = "sf.jpg", src = "la.jpg", src = "austin.jpg", src = "sydney.jpg");
+
+document.getElementById('city-type').onchange = changeCity;
 
 });
